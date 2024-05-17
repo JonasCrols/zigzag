ceafc736a4b001eade67a77e1d9e9a75ac0b97b3
Arne Symons <arnesymons@hotmail.com>
2024-05-17 11:23:53 +0200

    update lab3 architecture and mapping to yaml definition

--- lab3/main.py
+++ lab3/main.py
@@ -6,39 +6,40 @@ sys.path.insert(0, os.getcwd())
 from zigzag.api import get_hardware_performance_zigzag
 from zigzag.visualization.results.plot_cme import bar_plot_cost_model_evaluations_total
 
-from inputs.hardware.c_k import accelerator as accelerator_c_k
-from inputs.hardware.ox_k import accelerator as accelerator_ox_k
-from inputs.hardware.ox_fx_fy import accelerator as accelerator_ox_fx_fy
-from inputs.mapping.mapping_c_k import mapping as mapping_c_k
-from inputs.mapping.mapping_ox_k import mapping as mapping_ox_k
-from inputs.mapping.mapping_ox_fx_fy import mapping as mapping_ox_fx_fy
-
 
 # Path to the workload onnx model
 # onnx_model_path = "zigzag/inputs/examples/workload/resnet18.onnx"
 onnx_model_path = "lab1/resnet18_first_layer.onnx"
 
-# List of hardware architectures we run our experiment for
-hardwares = [accelerator_c_k, accelerator_ox_k, accelerator_ox_fx_fy]
-# List of mappings for each hardware (encodes the spatial dataflow)
-mappings = [mapping_c_k, mapping_ox_k, mapping_ox_fx_fy]
+# List of accelerators architectures we run our experiment for
+hardwares = [
+    "lab3/inputs/hardware/accelerator1.yaml",
+    "lab3/inputs/hardware/accelerator2.yaml",
+    "lab3/inputs/hardware/accelerator3.yaml",
+]
+# List of mappings for each accelerator
+mappings = [
+    "lab3/inputs/mapping/accelerator1.yaml",
+    "lab3/inputs/mapping/accelerator2.yaml",
+    "lab3/inputs/mapping/accelerator3.yaml",
+]
 
 cmes = []
-for (hardware, mapping) in zip(hardwares, mappings):
+for i, (hardware, mapping) in enumerate(zip(hardwares, mappings)):
     # Pickle filename to save list of cmes
-    pickle_filename = "lab3/outputs/list_of_cmes.pickle"
+    pickle_filename = f"lab3/outputs/list_of_cmes_{i}.pickle"
     # Call the zigzag api, using a provided accelerator and mapping
     energy, latency, results = get_hardware_performance_zigzag(
         onnx_model_path,
         hardware,
         mapping,
         opt="latency",
-        dump_filename_pattern=f"lab3/outputs/{hardware.name}.json",
+        dump_filename_pattern=f"lab3/outputs/accelerator{i}.json",
         pickle_filename=pickle_filename,
     )
     cmes.append(results[0][0])
 
-x_labels = [hardware.name for hardware in hardwares]
+x_labels = [f"accelerator{i}" for i in range(len(hardwares))]
 bar_plot_cost_model_evaluations_total(
     cmes,
     labels=x_labels,
